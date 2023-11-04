07256c1bb3d0af25bf0aca9fe382a6b7a142f848
Lim Zi Yang <yang7332@gmail.com>
2023-11-04 15:22:43 +0800

    feat: add font text style

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,5 +1,11 @@
 import React from "react";
+import styles from "./index.module.scss";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className={styles.homeSection}>
+      <text className={styles.name}>LIM ZI YANG</text>
+      <h1 className={styles.title}>Welcome</h1>
+    </div>
+  );
 }
