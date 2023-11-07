9b5b85c861fd8ff46eb724e59dc09f602cb94613
Lim Zi Yang <yang7332@gmail.com>
2023-11-07 20:32:20 +0800

    feat: implement smooth scroll function

---
+++ src/commons/utils.js
@@ -0,0 +1,27 @@
+export function smoothScrollTo(target, duration) {
+  const targetPosition = target.getBoundingClientRect().top;
+  const startPosition = window.scrollY;
+  const distance = targetPosition - startPosition;
+  let startTime = null;
+
+  function scrollAnimation(currentTime) {
+    if (startTime === null) {
+      startTime = currentTime;
+    }
+    const timeElapsed = currentTime - startTime;
+    const progress = timeElapsed / duration;
+
+    window.scrollTo(0, easeInOutCubic(progress) * distance + startPosition);
+
+    if (timeElapsed < duration) {
+      requestAnimationFrame(scrollAnimation);
+    }
+  }
+
+  function easeInOutCubic(t) {
+    if ((t *= 2) < 1) return 0.5 * t * t * t;
+    return 0.5 * ((t -= 2) * t * t + 2);
+  }
+
+  requestAnimationFrame(scrollAnimation);
+}
