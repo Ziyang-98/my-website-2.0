d08c11f3afb4a504fbb7373f24f8e16c5801159b
Lim Zi Yang <yang7332@gmail.com>
2024-02-28 17:04:25 +0800

    feat: implement footer with icons

--- src/sections/footer/index.js
+++ src/sections/footer/index.js
@@ -1,16 +1,36 @@
 import React from "react";
 import styles from "./index.module.scss";
+import { SocialIcon } from "react-social-icons";
+
+const SocialMediaIcon = ({ url }) => {
+  return (
+    <SocialIcon
+      className={styles["social-media-icon"]}
+      bgColor="#055f73"
+      fgColor="#6fbbbb"
+      href={url}
+      url={url}
+      target="_blank"
+    />
+  );
+};
 
 export default function Footer() {
   return (
     // <div className={styles["footer-section"]}>
     <div className={styles["footer-section"]}>
-      <div className={styles["social-medias-holder"]}>
-        <h1 className={styles.copyright}>Social medias</h1>
+      <div className={styles["social-media-holder"]}>
+        <h1 className={styles["social-media-name"]}>Lim Zi Yang</h1>
+        <div className={styles["social-media-icons-holder"]}>
+          <SocialMediaIcon url="https://www.facebook.com/lim.ziyang.12" />
+          <SocialMediaIcon url="https://www.linkedin.com/in/zi-yang-lim-a2b651188/" />
+          <SocialMediaIcon url="https://github.com/Ziyang-98" />
+          <SocialMediaIcon url="https://www.instagram.com/_papergoat_/" />
+        </div>
       </div>
       <div className={styles.divider} />
       <div className={styles["copyright-holder"]}>
-        <h3 className={styles.copyright}>Lim Zi Yang &#169; 2024</h3>
+        <h4 className={styles.copyright}>&#169; 2024 All Rights Reserved</h4>
       </div>
     </div>
   );
