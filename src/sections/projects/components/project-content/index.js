6cf098951528e3e01069a00fa68a4666e0e0ed5a
Lim Zi Yang <yang7332@gmail.com>
2024-07-04 21:08:29 +0800

    feat: add carousel to project content

--- src/sections/projects/components/project-content/index.js
+++ src/sections/projects/components/project-content/index.js
@@ -1,9 +1,16 @@
+import { Carousel, CarouselItem } from "../carousel";
 import styles from "./index.module.scss";
 
 export default function ProjectContent({ currentExpandedContent }) {
   return (
     <div className={styles["project-content-holder"]}>
-      <div className={styles["gallery-holder"]}>gallery area</div>
+      <div className={styles["gallery-holder"]}>
+        <Carousel height="100%">
+          <CarouselItem>Item 1</CarouselItem>
+          <CarouselItem>Item 2</CarouselItem>
+          <CarouselItem>Item 3</CarouselItem>
+        </Carousel>
+      </div>
       <div className={styles["details-holder"]}>{currentExpandedContent}</div>
     </div>
   );
