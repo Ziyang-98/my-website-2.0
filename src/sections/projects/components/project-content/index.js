0bef555fe1cc9301fdd841e68434aad0298fc982
Lim Zi Yang <yang7332@gmail.com>
2024-07-04 20:49:09 +0800

    feat: init holders within project content

--- src/sections/projects/components/project-content/index.js
+++ src/sections/projects/components/project-content/index.js
@@ -3,7 +3,8 @@ import styles from "./index.module.scss";
 export default function ProjectContent({ currentExpandedContent }) {
   return (
     <div className={styles["project-content-holder"]}>
-      <p>{currentExpandedContent}</p>
+      <div className={styles["gallery-holder"]}>gallery area</div>
+      <div className={styles["details-holder"]}>{currentExpandedContent}</div>
     </div>
   );
 }
