3dd614e1523381ec472ac989f91d6c7bd9032f1f
Lim Zi Yang <yang7332@gmail.com>
2024-07-08 20:37:10 +0800

    refactor: move close button into modal

--- src/sections/projects/components/modal/index.js
+++ src/sections/projects/components/modal/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import styles from "./index.module.scss";
 
-export default function Modal({ children }) {
+export default function Modal({ closeModal, children }) {
   return (
     <div id="modal-container" className={`${styles["modal-container"]}`}>
       <div className={styles["modal-background"]}>
-        <div className={styles.modal}>{children}</div>
+        <div className={styles.modal}>
+          {children}
+          <button className={styles["close-button"]} onClick={closeModal}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M13.414 12l5.293-5.293a1 1 0 0 0-1.414-1.414L12 10.586 6.707 5.293a1 1 0 0 0-1.414 1.414L10.586 12l-5.293 5.293a1 1 0 1 0 1.414 1.414L12 13.414l5.293 5.293a1 1 0 1 0 1.414-1.414L13.414 12z"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/sections/projects/components/project-grid/index.js
+++ src/sections/projects/components/project-grid/index.js
@@ -16,23 +16,8 @@ export default function ProjectGrid({ content }) {
   } = useProjects();
   return (
     <div className={styles["grid-container"]}>
-      <Modal>
+      <Modal closeModal={closeCard}>
         <ProjectContent currentExpandedContent={currentExpandedContent} />
-        <button className={styles["close-button"]} onClick={closeCard}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            // className={`${styles["exit-btn"]}`}
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            height="100%"
-            width="100%"
-          >
-            <path
-              fillRule="evenodd"
-              d="M13.414 12l5.293-5.293a1 1 0 0 0-1.414-1.414L12 10.586 6.707 5.293a1 1 0 0 0-1.414 1.414L10.586 12l-5.293 5.293a1 1 0 1 0 1.414 1.414L12 13.414l5.293 5.293a1 1 0 1 0 1.414-1.414L13.414 12z"
-            />
-          </svg>
-        </button>
       </Modal>
       {content.map((c, i) => (
         <ProjectCard
