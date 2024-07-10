6b6394405cdb7446e90271beafa347030d26d7e6
Lim Zi Yang <yang7332@gmail.com>
2024-07-10 22:58:47 +0800

    feat: implement project details holder layout

--- src/sections/projects/components/project-content/index.js
+++ src/sections/projects/components/project-content/index.js
@@ -1,5 +1,9 @@
 import { Carousel, CarouselItem } from "../carousel";
 import styles from "./index.module.scss";
+import ThemedLink from "components/themed-link";
+
+const sampleDescription =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vulputate velit ante, at consectetur metus posuere vitae. Aliquam accumsan, ipsum ut accumsan varius, nulla sem placerat metus, non dignissim ipsum ante id mi. Pellentesque arcu ipsum, fringilla id vestibulum non, auctor sit amet mauris. Nunc finibus efficitur nisi. Cras faucibus ex non ex finibus, vel fermentum tellus iaculis. Phasellus ac luctus diam. Vestibulum vel sapien eget orci vulputate laoreet quis ac libero. Phasellus ut ipsum est. Duis sit amet vulputate justo.";
 
 export default function ProjectContent({ currentExpandedContent }) {
   return (
@@ -13,7 +17,22 @@ export default function ProjectContent({ currentExpandedContent }) {
       </div>
       <div className={styles["details-holder"]}>
         <h1>Sample Title</h1>
-        <div>{currentExpandedContent}</div>
+        <div>{sampleDescription}</div>
+        <div>{sampleDescription}</div>
+        <div>{sampleDescription}</div>
+
+        <div className={styles["links-holder"]}>
+          <ThemedLink
+            href="https://www.google.com"
+            openNewTab={true}
+            iconSize={16}
+          >
+            <h4>Repository</h4>
+          </ThemedLink>
+          <ThemedLink href="https://www.google.com" iconSize={16}>
+            <h4>Project Info</h4>
+          </ThemedLink>
+        </div>
       </div>
     </div>
   );
