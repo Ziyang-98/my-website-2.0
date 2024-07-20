c769599ab07c2c1f5106401d64b686ebfb17d5e3
Lim Zi Yang <yang7332@gmail.com>
2024-07-20 15:55:23 +0800

    feat: change bg color of images in project preview

--- src/sections/projects/components/project-card/index.js
+++ src/sections/projects/components/project-card/index.js
@@ -9,7 +9,6 @@ export default function ProjectCard({ isExpanded, project, expandCard }) {
         <img
           className={styles["project-card-img"]}
           alt="sample"
-          // src="https://images.unsplash.com/photo-1682685797507-d44d838b0ac7?auto=format&fit=crop&q=80&w=1470&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           src={project.image.preview}
           width={project.previewSize.width}
           height={project.previewSize.height}
