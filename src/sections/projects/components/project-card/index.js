cd81d8950ebe36f140c60430f3b6e890cd5c67c8
Lim Zi Yang <yang7332@gmail.com>
2024-07-24 21:33:12 +0800

    feat: fix aspect ratio of previews

--- src/sections/projects/components/project-card/index.js
+++ src/sections/projects/components/project-card/index.js
@@ -10,8 +10,8 @@ export default function ProjectCard({ isExpanded, project, expandCard }) {
           className={styles["project-card-img"]}
           alt="sample"
           src={project.image.preview}
-          width={project.previewSize.width}
-          height={project.previewSize.height}
+          // width={project.previewSize.width}
+          // height={project.previewSize.height}
         />
       </div>
       <div className={styles["project-info-holder"]}>
