0346dd66bbfa022aa768e9308a4ccd7d3a1b3007
Lim Zi Yang <yang7332@gmail.com>
2024-11-03 23:38:18 +0800

    refactor: shorten linkedin link

--- src/sections/footer/index.js
+++ src/sections/footer/index.js
@@ -27,7 +27,7 @@ export default function Footer() {
               <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
             </svg>
           </ThemedLinkButton>
-          <ThemedLinkButton url="https://www.linkedin.com/in/zi-yang-lim-a2b651188/">
+          <ThemedLinkButton url="https://www.linkedin.com/in/ziyang-lim98/">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width={ICON_SIZE}
