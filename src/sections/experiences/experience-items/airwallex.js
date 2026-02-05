c72d1c2ac46550b4a1795c0833c6f147a0688af2
Lim Zi Yang <yang7332@gmail.com>
2026-02-06 00:15:04 +0800

    feat: update jobscope

--- src/sections/experiences/experience-items/airwallex.js
+++ src/sections/experiences/experience-items/airwallex.js
@@ -2,8 +2,8 @@ const Airwallex = {
   jobTitle: "Software Engineer II",
   company: "Airwallex",
   duration: "Mar 2025 - Present",
-  description: "Working as a Front-End Engineer in the Growth & Ecosystem team",
-  jobscope: ["Working as a Frontend Engineer in the Growth & Ecosystem team"],
+  description: "Working as a Software Engineer in the Growth team",
+  jobscope: ["Working as a Software Engineer in the Growth team"],
   funFact:
     "My first day coincided with the opening of the new office! Got to experience an exciting lion dance and a donut buffet!",
 };
